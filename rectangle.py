ceb021e807e78a7819301e6769160ee2fd9c0f77
Ubuntu <ubuntu@ip-172-31-27-17.ec2.internal>
2019-09-13 17:18:19 +0000

    second try

---
+++ rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Whitten
+# Created on: September 2019
+# This is a program which can find the area and perimeter or a rectangle.
+
+
+# This is what finds the area and perimeter of the rectangle.
+# Replace the two numbers at the end of lines 12, 14 and 15 to continue.
+def main():
+    print("If a rectangle has the demensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Then the area of the rectangle is {}cm^2" .format(5*3))
+    print("and the perimeter is {}cm" .format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
